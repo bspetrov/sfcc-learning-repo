c4d2cbdec19e833a87e4e45473d7fdd466930376
kleinerspecht <bspetrovv@gmail.com>
2022-08-05 00:12:03 +0300

    Created review form

--- storefront-reference-architecture-6.1.0/cartridges/bp_storefront_reviews/cartridge/controllers/Product.js
+++ storefront-reference-architecture-6.1.0/cartridges/bp_storefront_reviews/cartridge/controllers/Product.js
@@ -4,6 +4,9 @@ var server = require('server');
 var page = module.superModule;
 server.extend(page);
 
+var CustomObjectMgr = require('dw/object/CustomObjectMgr');
+var URLUtils = require('dw/web/URLUtils');
+
 
 /**
  * Product-GiveReview : This endpoint is when the shopped selects "Give review" on a product he has ordered already.
@@ -13,9 +16,16 @@ server.extend(page);
 
 server.get(
     "GiveReview",
+    server.middleware.https,
     function (req, res, next) {
+        var actionUrl = dw.web.URLUtils.url('Product-GiveReviewHandler');
+        var reviewForm = server.forms.getForm('review');
+        reviewForm.clear();
 
-
+        res.render('review/writeReview', {
+            actionUrl: actionUrl,
+            reviewForm: reviewForm
+        });
 
         next();
 
@@ -25,9 +35,25 @@ server.get(
 
 server.post(
     "GiveReviewHandler",
+    server.middleware.https,
     function (req, res, next) {
+        var reviewForm = server.forms.getForm('review');
+        var continueUrl = dw.web.URLUtils.utl('Home-Show');
+        
+        // TODO -> Process form and create the custom object
 
-
+        if (reviewForm.valid) {
+            try {
+                var reviewCustomObject = CustomObjectMgr.createCustomObject('ProductReview', )
+            } catch (e) {
+                var err = e;
+                res.setStatusCode(500);
+                res.json({
+                    error: true,
+                    redirectUrl: URLUtils.url('Error-Start').toString()
+                });
+            }
+        }
         next();
     }
 );
