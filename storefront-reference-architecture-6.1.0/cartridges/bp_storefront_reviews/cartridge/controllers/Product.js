40417301c559d518985f75e3e84fe3461fd06796
kleinerspecht <bspetrovv@gmail.com>
2022-08-05 20:40:56 +0300

    Some work in the GiveReview endpoint - order history querying and etc..

--- storefront-reference-architecture-6.1.0/cartridges/bp_storefront_reviews/cartridge/controllers/Product.js
+++ storefront-reference-architecture-6.1.0/cartridges/bp_storefront_reviews/cartridge/controllers/Product.js
@@ -6,6 +6,7 @@ server.extend(page);
 
 var CustomObjectMgr = require('dw/object/CustomObjectMgr');
 var URLUtils = require('dw/web/URLUtils');
+var OrderMgr = require('dw/order/OrderMgr');
 
 
 /**
@@ -18,15 +19,54 @@ server.get(
     "GiveReview",
     server.middleware.https,
     function (req, res, next) {
-        var actionUrl = dw.web.URLUtils.url('Product-GiveReviewHandler');
-        var reviewForm = server.forms.getForm('review');
-        reviewForm.clear();
+        if (req.currentCustomer.profile) {
+            try {
+                var customerNo = req.currentCustomer.profile.customerNo;
+                var customerOrdersQuery = OrderMgr.queryOrders('customerNo = {0}', null, customerNo);
+                var customerProducts = [];
+                var totalOrders = customerOrdersQuery.hasNext();
+                if (totalOrders) {
+                    while (customerOrdersQuery.hasNext()) {
+                        var order = customerOrdersQuery.next();
+                        var orderItems = order.productLineItems;
+                        for (var item in orderItems) {
+                            var product = orderItems[item].product;
+                            var productID = null;
+                            if (product.variant == true) {
+                                productID = product.masterProduct.ID;
+                                customerProducts.push(productID);
+                            } else {
+                                productID = product.ID;
+                                customerProducts.push(productID);   
+                            }
+                        }
+                    }
 
-        res.render('review/writeReview', {
-            actionUrl: actionUrl,
-            reviewForm: reviewForm
-        });
+                } else {
+                    res.json({
+                        "ERROR": 'You have no orders!'
+                    });
+                }
+                var requestID = req.querystring.id;
+                if (customerProducts.includes(requestID)) {
+                    var actionUrl = dw.web.URLUtils.url('Product-GiveReviewHandler');
+                    var reviewForm = server.forms.getForm('review');
+                    reviewForm.clear();
+            
+                    res.render('review/writeReview', {
+                        actionUrl: actionUrl,
+                        reviewForm: reviewForm
+                    });
+                } else {
+                    res.json({
+                        "You havent purchased this product!"
+                    })
+                }
+            } catch (e) {
+                //
+            }
 
+        }
         next();
 
     }
--- storefront-reference-architecture-6.1.0/cartridges/bp_storefront_reviews/cartridge/controllers/Tile.js
+++ storefront-reference-architecture-6.1.0/cartridges/bp_storefront_reviews/cartridge/controllers/Tile.js
@@ -17,19 +17,7 @@ server.append(
     function(req, res, next) {
         var viewData = res.getViewData();
         if (req.currentCustomer.profile) {
-            var customerNo = req.currentCustomer.profile.customerNo;
-            var customerOrdersQuery = OrderMgr.queryOrders('customerNo = {0}', null, customerNo);
-            var customerProducts = [];
-
-            while (customerOrdersQuery.hasNext()) {
-                var order = customerOrdersQuery.next();
-                var orderItems = order.productLineItems;
-                for (var item in orderItems) {
-                    var productId = orderItems[item].productID;
-                    customerProducts.push(productId)
-                }
-            }
-            viewData.reviewUrl = dw.web.URLUtils.url('Product-GiveReview')
+            viewData.reviewUrl = dw.web.URLUtils.url('Product-GiveReview');
             viewData.loggedIn = true;
         }
         res.setViewData(viewData);
